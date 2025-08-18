9c045461036265171800b55b3e24e6c21f4aceb3
Elvira Latypova <ellalatypova666@gmail.com>
2025-08-18 23:55:42 +0500

    style: inintial base styles

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -5,6 +5,8 @@ import { RouterProvider } from "react-router";
 
 import { appRouter } from "./router";
 
+import "./styles/index.scss";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={appRouter()} />
